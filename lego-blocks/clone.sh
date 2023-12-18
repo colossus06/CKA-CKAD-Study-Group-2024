c3767e2fc644065ebc2d52d6cbfc7275e3e9846e
colossus06 <denizkavuk86@gmail.com>
2023-12-18 18:33:20 +0300

    edit: taint

--- lego-blocks/clone.sh
+++ lego-blocks/clone.sh
@@ -2,6 +2,8 @@
 git clone https://github.com/colossus06/cka-ckad-study-group-2024.git
 cd cka-ckad-study-group-2024/lego-blocks/
 chmod +x ./
+kubectl taint nodes control-plane node-role.kubernetes.io/master-
+echo "untainted master node"
 ./ns.sh
 ./monitoring-stack.sh
 sleep 8
